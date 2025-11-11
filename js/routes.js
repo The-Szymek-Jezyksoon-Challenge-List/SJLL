6488fd809d6d168426014293293dfa3f239783cb
lubiegry353 <szymonkurp3@gmail.com>
2025-11-11 14:57:44 +0100

    Add Packs route to routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import Packs from './pages/packs.js'; // 👈 DODAJ TO
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
+    { path: '/packs', component: Packs }, // 👈 I TO
 ];
