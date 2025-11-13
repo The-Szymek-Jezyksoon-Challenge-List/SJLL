dc6f3b57583ace2ca0a5ef70d1f1fd2a548dea70
lubiegry353 <szymonkurp3@gmail.com>
2025-11-13 13:08:26 +0100

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,11 +1,10 @@
-import List from './pages/List.js';
-import Leaderboard from './pages/Leaderboard.js';
-import Roulette from './pages/Roulette.js';
-import Packs from './pages/packs.js'; // 👈 DODAJ TO
+import routes from './routes.js';
 
-export default [
-    { path: '/', component: List },
-    { path: '/leaderboard', component: Leaderboard },
-    { path: '/roulette', component: Roulette },
-    { path: '/packs', component: Packs }, // 👈 I TO
-];
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes,
+});
+
+const app = Vue.createApp({});
+app.use(router);
+app.mount('#app');
