b16c3ee0a39b642da3e5ec6805a3f2e7b7b689d4
lubiegry353 <szymonkurp3@gmail.com>
2025-11-13 13:18:36 +0100

    Update packs.js

--- js/pages/packs.js
+++ js/pages/packs.js
@@ -1,8 +1,35 @@
 export default {
+  data() {
+    return {
+      packs: [
+        {
+          name: 'Starter Pack',
+          levels: ['18stka'],
+        },
+        {
+          name: 'Advanced Pack',
+          levels: ['aod'],
+        },
+        {
+          name: 'Challenge Pack',
+          levels: ['egzamin'],
+        },
+      ],
+      selectedPack: null,
+    };
+  },
+  methods: {
+    openPack(pack) {
+      this.selectedPack = pack;
+    },
+    backToPacks() {
+      this.selectedPack = null;
+    },
+  },
   template: `
-    <div style="color:white; text-align:center; padding:40px;">
-      <h1>Packs działa 🎉</h1>
-      <p>Router działa, komponent się załadował poprawnie!</p>
-    </div>
-  `
-};
+    <section class="packs" style="color:white; text-align:center; padding:20px;">
+      <h1 style="margin-bottom:20px;">🎮 Packs</h1>
+
+      <div v-if="!selectedPack">
+        <div v-for="pack in packs"
+             :key="pack.name"
