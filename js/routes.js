6c08f32e0db5edde73b2fac81ff06ca1cd338416
lubiegry353 <szymonkurp3@gmail.com>
2025-11-15 14:44:32 +0100

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -2,10 +2,12 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import Packs from './pages/packs.js';
+import Level from './pages/Level.js'; // ⬅ NOWE
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/packs', component: Packs },
+    { path: '/level/:id', component: Level, props: true }, // ⬅ NOWE
 ];
